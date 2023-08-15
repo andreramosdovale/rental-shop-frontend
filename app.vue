875807193593c25ab3ba94aaac73a754d0112ea9
André Ramos <andre_ramosdovale@outlook.com>
2023-08-14 21:46:44 -0300

    fix: ajuste em codigo eslint

--- app.vue
+++ app.vue
@@ -6,7 +6,9 @@
       <v-card-subtitle>This is a subtitle</v-card-subtitle>
       <v-card-text> This is content </v-card-text>
     </v-card>
-    <v-btn class="mt-10">Hello Sass Changes</v-btn>
+    <v-btn class="mt-10">
+      Hello Sass Changes
+    </v-btn>
   </div>
 </template>
 
